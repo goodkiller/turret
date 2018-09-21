767a4b45bd7603512e2639e38de4bbba5f3b84d9
markopraakli <markopraakli@gmail.com>
2018-09-21 17:27:50 +0300

    Web

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const { 
 	cfg, 
+	colors,
 	cv, 
 	web
 } = require('./lib/init');
@@ -13,22 +14,29 @@ const {
 
 grabFrames((frame) => {
 
-	const frameHLS = frame.cvtColor(cv.COLOR_BGR2HSV);
+	const frameHSV = frame.cvtColor(cv.COLOR_BGR2HSV);
+
+	// Stream picture
+	if( cfg.stream_picture )
+	{
+		console.log('[VIDEO] Sending picture ...');
+
+		web.io.emit('picture', cv.imencode('.png', frame).toString('base64') );
+
+		cfg.stream_picture = false;
+	}
 
 	const crosshair = drawCrosshair( frame );
 
-	const brownLower = new cv.Vec(0, 192, 98);
-	const brownUpper = new cv.Vec(179, 255, 255);
-
-	const rangeMask = frameHLS.inRange(brownLower, brownUpper);
+	const rangeMask = frameHSV.inRange(
+		new cv.Vec(colors[0][0], colors[0][1], colors[0][2]),
+		new cv.Vec(colors[1][0], colors[1][1], colors[1][2])
+	);
 
 	const blurred = rangeMask.blur(new cv.Size(10, 10));
 	const thresholded = blurred.threshold(100, 255, cv.THRESH_BINARY);
 
 	drawTarget(thresholded, frame, cfg.minPxSize, crosshair);
-
-	// Send frames to web
-	//web.io.emit('frame', cv.imencode('.jpg', frame).toString('base64') );
 
 	if( cfg.guiEnabled )
 	{
